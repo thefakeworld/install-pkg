bcb6f5f9ee0a5510fb6ca78a895da63b70fb6ebb
huyu <huyu@hzanchu.com>
2023-08-22 16:20:00 +0800

    feat: just write something

---
+++ src/demo.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const onClick = () => {
+  // console.log('saglsdkgj')
+  const a = 1
+  // alert(a + 2)
+}
+</script>
+
+<template>
+  <div class=" sdag">
+    aasdgd
+  </div>
+</template>
